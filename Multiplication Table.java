36b8ac238a53e2ff2088229947c68f434287a49b
KRISHNA <krish7cbe@gmail.com>
2022-10-30 21:54:34 +0530

    Create Multiplication Table.java
    Creating Multiplication Table using java

---
+++ Multiplication Table.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    int a;
+	    int i;
+	    for(i=1;i<=10;i++)
+	    {
+	        a=n*i;
+	     //System.out.println(a);   
+	     System.out.println(n+"*"+i+"="+a);
+	    }
+		
+	}
+}
+
